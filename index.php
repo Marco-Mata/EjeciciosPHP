6d0d17df458129d6159592af39fa43b58a811af5
Marco-Mata <57307745+Marco-Mata@users.noreply.github.com>
2020-05-31 15:30:07 -0600

    cambio en referencias
    se agregan las que seran las referencias de los otros ejercicios

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-<?php
-    $txt= "Hello Word";
-    $x=5;
-    $y=10.5;
-?>
+<!DOCTYPE html>
+<html>
+<body>
+
+    <h1>Ejercicios de PHP </h1>
+
+    <ul>
+        <li><a href="ejercicio_2.php" target="_new">Ejercicio 2</a></li>
+    </ul>
+</body>
+</html>
