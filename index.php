5aa4bc28527625a6cb8a872fda362ca94c0176fe
Marco-Mata <57307745+Marco-Mata@users.noreply.github.com>
2020-06-02 19:53:19 -0600

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
         <li><a href="ejercicio_3.php" target="_new">Ejercicio 3</a></li>
         <li><a href="ejercicio_4.php" target="_new">Ejercicio 4</a></li>
         <li><a href="ejercicio_5.php" target="_new">Ejercicio 5</a></li>
-        <li><a href="ejercicio_6.php" target="_new">Ejercicio 5</a></li>
-        <li><a href="ejercicio_7.php?clase = lenguaje 4&periodo=2020-21" target="_new">Ejercicio 6</a></li>
+        <li><a href="ejercicio_6.php" target="_new">Ejercicio 6</a></li>
+        <li><a href="ejercicio_7.php?clase = lenguaje 4&periodo=2020-21" target="_new">Ejercicio 7</a></li>
 
     </ul>
 </body>
