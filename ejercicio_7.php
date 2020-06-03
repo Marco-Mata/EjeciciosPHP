af5024d323b263666f6c96c8d10ed81679ad1f39
Marco-Mata <57307745+Marco-Mata@users.noreply.github.com>
2020-06-02 22:35:53 -0600

    Update ejercicio_7.php

--- ejercicio_7.php
+++ ejercicio_7.php
@@ -3,21 +3,13 @@
 <body>
 
 <h1>Ejercicio 7 Numeros Primos</h1>
-
-    
-    <form>
-        Escribe un numero:
-        <input name ="numero" type ="text" placeholder = "Escribe cualquier numero">
-        
-        <input type="submit" value ="Registrar">
-    </form>
-    <?php
+<?php
         header ("content-type: text/html;charset=\"utf-8\"");
 
         echo "<h1>".$_GET['numero']."</h1>";
 
         if (is_numeric($_GET['numero']) && $_GET['numero'] >=1){
-            if(($_GET['numero']%2)!=0){
+            if($_GET['numero']%2!=0){
                 echo "<h2>Es un numero primo".$_GET['numero']."</h2>";
             }         
             
@@ -31,6 +23,14 @@
 
 
     ?>
+    
+    <form>
+        Escribe un numero:
+        <input name ="numero" type ="text" placeholder = "Escribe cualquier numero">
+        
+        <input type="submit" value ="Registrar">
+    </form>
+    
 
 </body>
 </hmtl>
