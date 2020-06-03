1d26894ae425026e63dec663dc487d65c276d1ce
Marco-Mata <57307745+Marco-Mata@users.noreply.github.com>
2020-06-02 19:39:24 -0600

    correcion
    se quito signo mayor que, en el codigo que no iba

--- ejercicio_6.php
+++ ejercicio_6.php
@@ -12,14 +12,14 @@
             y el lado del servidor
         */
 
-        echo"<h1>".$_GET['clase']."></h1>";
-        echo"<h2>".$_GET['periodo']."></h2>";
+        echo"<h1>".$_GET['clase']."</h1>";
+        echo"<h2>".$_GET['periodo']."</h2>";
 
-        echo"<h1>".$_GET['nombre']."></h1>";
-        echo"<h2>".$_GET['apellido']."></h2>";
+        echo"<h1>".$_GET['nombre']."</h1>";
+        echo"<h2>".$_GET['apellido']."</h2>";
 
-        if(is_numeric($_GET['edad']) && $_GET['edad']>=1);{
-            echo"<h3>Bienvenido ".$_GET['nombre']." ".$_GET['apellido']."</h3>";
+        if(is_numeric($_GET['edad']) && $_GET['edad'] >=1);{
+            echo"<h3>Bienvenido".$_GET['nombre']." ".$_GET['apellido']."</h3>";
 
         }
         else{
