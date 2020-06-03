2e994a8a30c39ee08cf1e774885d76cc274bf761
Marco-Mata <57307745+Marco-Mata@users.noreply.github.com>
2020-06-02 22:17:50 -0600

    logica
    se cambio la logica

--- ejercicio_7.php
+++ ejercicio_7.php
@@ -4,33 +4,34 @@
 
 <h1>Ejercicio 7 Numeros Primos</h1>
 
-    <?php
-        header ("content-type: text/html;charset=\"utf-8\"");
-
-        echo "<h1>".$_GET['numero']."<\h1>";
-
-        if (is_numeric($_GET['numero']) && $_GET['numero'] >=1){
-            if($_GET['numero']%2!=0){
-                echo "<h2>Es un numero primo".$_GET['numero']."<\h2>";
-            }         
-            
-            else{
-                echo "<h2>Es no es numero primo".$_GET['numero']."<\h2>";
-            }
-        }
-        else{
-            echo "<h2>Ingrese un numero mayor que cero".$_GET['numero']."<\h2>";
-        }
-
-
-    ?>
-
+    
     <form>
         Escribe un numero:
         <input name ="numero" type ="text" placeholder = "Escribe cualquier numero">
         
         <input type="submit" value ="Registrar">
     </form>
+    <?php
+        header ("content-type: text/html;charset=\"utf-8\"");
+
+        echo "<h1>".$_GET['numero']."</h1>";
+
+        if (is_numeric($_GET['numero']) && $_GET['numero'] >=1){
+            if($_GET['numero']/$_GET['numero'] && $_GET['numero']/1){
+                echo "<h2>Es un numero primo".$_GET['numero']."</h2>";
+            }         
+            
+            else{
+                echo "<h2>Es no es numero primo".$_GET['numero']."</h2>";
+            }
+        }
+        else{
+            echo "<h2>Ingrese un numero mayor que cero".$_GET['numero']."</h2>";
+        }
+
+
+    ?>
+
 
 
 </body>
