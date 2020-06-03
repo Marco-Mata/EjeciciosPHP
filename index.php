ba53c05ac8926ad405a66c0b0ca61d4555ecee32
Marco-Mata <57307745+Marco-Mata@users.noreply.github.com>
2020-06-02 19:14:02 -0600

    links
    se agregaron los links de los demas ejercicios

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
 
     <ul>
         <li><a href="ejercicio_2.php" target="_new">Ejercicio 2</a></li>
+        <li><a href="ejercicio_3.php" target="_new">Ejercicio 3</a></li>
+        <li><a href="ejercicio_4.php" target="_new">Ejercicio 4</a></li>
+        <li><a href="ejercicio_5.php" target="_new">Ejercicio 5</a></li>
+        <li><a href="ejercicio_6.php?clase = lenguaje 4&periodo=2020-21" target="_new">Ejercicio 6</a></li>
+
     </ul>
 </body>
 </html>
