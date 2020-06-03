a7f761234f0be2864910e27f05e45f901896bc8f
Marco-Mata <57307745+Marco-Mata@users.noreply.github.com>
2020-06-02 21:16:45 -0600

    Update ejercicio_7.php

--- ejercicio_7.php
+++ ejercicio_7.php
@@ -7,7 +7,7 @@
     <?php
         header ("content-type: text/html;charset=\"utf-8\"");
 
-        echo"<h1>".$_GET['numero']."<\h1">;
+        echo"<h1>".$_GET['numero']."<\h1>";
 
         if (is_numeric($_GET['numero']) && $_GET['numero'] =>1){
             if($_GET['numero']%2==0){
